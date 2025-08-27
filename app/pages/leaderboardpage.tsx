243a33b15001a1c32504b744d629bce1679133fc
Ishita Sati <ishasati19@gmail.com>
2025-08-27 22:48:11 +0530

    Create leaderboardpage.tsx

---
+++ app/pages/leaderboardpage.tsx
@@ -0,0 +1,11 @@
+export default function LeaderboardPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-2xl font-semibold text-eco-600">Leaderboard</h1>
+      <p className="mt-4 text-gray-600">
+        Check out the top eco-warriors contributing to sustainability 🌍
+      </p>
+      {/* Later you can fetch leaderboard data here */}
+    </main>
+  );
+}
