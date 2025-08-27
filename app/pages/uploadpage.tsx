ffcfffe6492aa6263d536736d3b67b7be18298c5
Ishita Sati <ishasati19@gmail.com>
2025-08-27 22:47:42 +0530

    Create uploadpage.tsx

---
+++ app/pages/uploadpage.tsx
@@ -0,0 +1,11 @@
+export default function UploadPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-2xl font-semibold text-eco-600">Upload Waste</h1>
+      <p className="mt-4 text-gray-600">
+        Submit your waste details here to earn eco credits 🌱
+      </p>
+      {/* You can add a form or file upload input here later */}
+    </main>
+  );
+}
