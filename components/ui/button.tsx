a81ed76ae869421e07b652a99e0b653f95a61393
Ishita Sati <ishasati19@gmail.com>
2025-08-29 00:22:27 +0530

    Create button.tsx

---
+++ components/ui/button.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button
+      className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
