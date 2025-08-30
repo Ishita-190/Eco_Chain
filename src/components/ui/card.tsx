6aad7eede535becfa678f32c95a0094dbd392e20
Ishita Sati <ishasati19@gmail.com>
2025-08-31 02:39:28 +0530

    Update card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "@/lib/utils"
+import { cn } from "@/src/lib/utils"
 
 const Card = React.forwardRef<
   HTMLDivElement,
@@ -80,3 +80,4 @@ export {
   CardContent,
   CardFooter,
 }
+
