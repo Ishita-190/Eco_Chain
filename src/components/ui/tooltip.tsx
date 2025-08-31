48c4cf62664742d54be3be0c30e7c2bd0a21d91b
Ishita Sati <ishasati19@gmail.com>
2025-08-31 13:45:19 +0530

    Create tooltip.tsx

---
+++ src/components/ui/tooltip.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+
+export function TooltipProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <TooltipPrimitive.Provider delayDuration={300}>
+      {children}
+    </TooltipPrimitive.Provider>
+  );
+}
