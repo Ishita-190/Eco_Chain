d0aebd5d7dbbc38dd5a9989ee2d31115e6a9fe13
Ishita Sati <ishasati19@gmail.com>
2025-08-31 13:54:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Toaster } from "sonner";
 import { TooltipProvider } from "@/src/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Index from "@/src/app/Index";
+import NotFound from "@/src/app/NotFound";
 
 const queryClient = new QueryClient();
 
