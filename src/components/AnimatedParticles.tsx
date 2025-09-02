f3b3bc52869d68383594499213ce150dcfd39338
Ishita Sati <ishasati19@gmail.com>
2025-09-02 16:16:36 +0530

    Update AnimatedParticles.tsx

--- src/components/AnimatedParticles.tsx
+++ src/components/AnimatedParticles.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
@@ -65,3 +67,4 @@ export default function AnimatedParticles() {
     </div>
   );
 }
+
