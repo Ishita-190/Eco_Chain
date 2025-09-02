2a2e4a379c76d7db7f8ad9f70b314c3997136c8c
Ishita Sati <ishasati19@gmail.com>
2025-09-02 16:12:38 +0530

    Update WasteCounter.tsx

--- src/components/WasteCounter.tsx
+++ src/components/WasteCounter.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
@@ -47,3 +49,4 @@ export default function WasteCounter() {
     </motion.div>
   );
 }
+
