d778606f7d386aa066c6331a8665ef98532895c9
Ishita Sati <ishasati19@gmail.com>
2025-09-03 23:28:23 +0530

    Create select.tsx

---
+++ src/components/ui/select.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { cn } from "@/src/lib/utils";
+
+export interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(({ className, children, ...props }, ref) => {
+  return (
+    <select
+      ref={ref}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-gray-600 bg-gray-700 px-3 py-2 text-sm text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </select>
+  );
+});
+Select.displayName = "Select";
+
+export { Select };
