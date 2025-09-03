a88398716bfa968336c02974a82dbc7eee074b2b
Ishita Sati <ishasati19@gmail.com>
2025-09-03 17:13:17 +0530

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,3 +1,4 @@
+"use client"
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 import { cn } from "@/src/lib/utils"
@@ -50,4 +51,5 @@ const TabsContent = React.forwardRef<
 TabsContent.displayName = TabsPrimitive.Content.displayName
 
 export { Tabs, TabsList, TabsTrigger, TabsContent }
+
 
