1e5df1b609a822f670538f0923165c6dd6893c7f
Ishita Sati <ishasati19@gmail.com>
2025-09-04 15:30:57 +0530

    Update marquee.tsx

--- src/components/magicui/marquee.tsx
+++ src/components/magicui/marquee.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/src/lib/utils";
+import { cn } from "@/lib/utils";
 import { ComponentPropsWithoutRef } from "react";
 
 interface MarqueeProps extends ComponentPropsWithoutRef<"div"> {
@@ -6,7 +6,7 @@ interface MarqueeProps extends ComponentPropsWithoutRef<"div"> {
   reverse?: boolean;
   pauseOnHover?: boolean;
   children: React.ReactNode;
-  vertical?: boolean;
+  vertical?: boolean; // still supported
   repeat?: number;
 }
 
@@ -23,26 +23,31 @@ export function Marquee({
     <div
       {...props}
       className={cn(
-        "group overflow-hidden p-2 [--duration:40s] [--gap:1rem]",
-        vertical ? "flex flex-col" : "flex flex-row",
+        "group overflow-hidden [--duration:40s] [--gap:1rem]",
         className
       )}
     >
-      {Array(repeat)
-        .fill(0)
-        .map((_, i) => (
-          <div
-            key={i}
-            className={cn(
-              "flex shrink-0 justify-start [gap:var(--gap)]",
-              vertical ? "animate-marquee-vertical flex-col" : "animate-marquee flex-row",
-              pauseOnHover && "group-hover:[animation-play-state:paused]",
-              reverse && "[animation-direction:reverse]"
-            )}
-          >
-            {children}
-          </div>
-        ))}
+      <div
+        className={cn(
+          "flex shrink-0 whitespace-nowrap",
+          vertical ? "flex-col animate-marquee-vertical" : "flex-row animate-marquee",
+          pauseOnHover && "group-hover:[animation-play-state:paused]",
+          reverse && "[animation-direction:reverse]"
+        )}
+      >
+        {Array(repeat)
+          .fill(0)
+          .map((_, i) =>
+            React.Children.map(children, (child, idx) => (
+              <span
+                key={`${i}-${idx}`}
+                className={vertical ? "block my-2" : "inline-block mx-8"}
+              >
+                {child}
+              </span>
+            ))
+          )}
+      </div>
     </div>
   );
 }
